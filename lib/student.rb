9825568d619be4783979b2983f39d4b574c2e9a0
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 19:01:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,15 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    first_X_students_in_grade_10(1)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    row = DB[:conn].execute(sql)[0]
+    self.new_from_db(row)
   end
 
   def self.find_by_name(name)
