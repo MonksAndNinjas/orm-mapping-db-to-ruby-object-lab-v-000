7a1a7c5bddc160d3800146b5444fe3714f4f3542
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 18:58:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,8 +68,9 @@ class Student
       LIMIT 1
     SQL
 
-    self.new_from_db(DB[:conn].execute(sql)[0])
-
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
