d4fd9803699483159bd7620a102c6067b969752d
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 19:08:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql,x).map do |row|
+    DB[:conn].execute(sql,number).map do |row|
       self.new_from_db(row)
     end
   end
@@ -73,6 +73,7 @@ class Student
   end
 
   def self.all_students_in_grade_X(grade)
+  end
 
   def self.find_by_name(name)
     # find the student in the database given a name
