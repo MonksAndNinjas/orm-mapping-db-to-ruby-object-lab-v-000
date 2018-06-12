914b923cd71e705eda5c5954badeba3b21bf5c84
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-12 19:08:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -71,6 +71,8 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(grade)
 
   def self.find_by_name(name)
     # find the student in the database given a name
